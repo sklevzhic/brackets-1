875ca4b211d4804101304be9553d750efead4a2a
Alexander Klevzhits <sklevzhic@gmail.com>
2020-03-03 23:17:19 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    while (str.length !== 0) {
+        let isRemove = false;
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            const template = bracketsConfig[+i].join('');
+            if (~str.indexOf(template)) {
+                str = str.replace(template, '');
+                isRemove = true;
+            }
+        }
+        if (!isRemove) {
+            return false;
+        }
+    }
+    return true;
 }
